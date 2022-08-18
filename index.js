f6b7c82f8397926bc63df7759bf1cbb5d7595452
moringa <oscarokumu47@gmail.com>
2022-08-18 19:32:36 +0300

    modify index.js && package-lock.json

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your solution in this file!
+var customerName = 'bob';
+let bestCustomer = "not bob";
+
+function upperCaseCustomerName(){
+    return customerName.toUpperCase();
+}
+console.log(upperCaseCustomerName());
+
+function setBestCustomer(){
+   return bestCustomer;
+}
+console.log(setBestCustomer());
